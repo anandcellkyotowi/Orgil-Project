50faeec55d7e675b6efaec17e71e19ad10c17e76
Uilsee <Munkh-Uils_@LEAP-22s-iMac.local>
2022-04-05 17:21:32 +0800

    inv

--- Inventory.js
+++ Inventory.js
@@ -1,8 +1,20 @@
+const canvas = document.getElementById("canvas");
+canvas.width = 800;
+canvas.height = 560;
+
+const inv = canvas.getContext("2d");
+
 class Inventory {
-  draw() {
-
+  constructor() {
+    this.width = 300;
+    this.height = 70;
   }
-  insertItem({}) {
-
+  draw() {
+    inv.fillStyle = "grey";
+    inv.fillRect(490, 10, this.width, this.height);
   }
 }
+
+const inventory = new Inventory();
+
+inventory.draw();
