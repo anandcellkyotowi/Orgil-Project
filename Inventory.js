ef0c2b85899c208c40c36a91b4c5c516729cf49d
Monho12 <oorgilmojo@gmail.com>
2022-04-06 14:09:40 +0800

    itemsPatch1.0

--- Inventory.js
+++ Inventory.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Inventory {
   constructor() {
     this.position = {
@@ -13,29 +12,3 @@ class Inventory {
     ctx.fillRect(this.position.x, this.position.y , this.width, this.height);
   }
 }
-=======
-const canvas = document.querySelector("canvas");
-
-const ctx = canvas.getContext("2d");
-
-canvas.width = 800;
-canvas.height = 560;
-
-class Inventory {
-  constructor() {
-    this.position = {
-      x: 0,
-      y: 0,
-    };
-    this.width = 300;
-    this.height = 70;
-  }
-  draw() {
-    inv.fillStyle = "green";
-    inv.fillRect(490, 10, this.width, this.height);
-  }
-}
-
-const inventory = new Inventory();
-inventory.draw();
->>>>>>> 3ac9667fb2d7e903f8ab28523c619bde8bac5290
--- Item.js
+++ Item.js
@@ -1,6 +1,6 @@
 const gravity = 1.5;
 
-class Items {
+class Filter {
   constructor({ x, y }) {
     this.position = {
       x,
@@ -8,6 +8,8 @@ class Items {
     };
     this.width = 50;
     this.height = 50;
+    this.image = new Image();
+    this.image.src = "./img/filter.png"
 
     this.velocity = {
       x: 0,
@@ -15,13 +17,84 @@ class Items {
     };
   }
   draw() {
-    ctx.fillStyle = "green";
-    ctx.fillRect(
+    ctx.drawImage(
+      this.image,
       this.position.x,
       this.position.y,
       this.width,
       this.height,
-      this.color
+    );
+  }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
+}
+class Branch {
+  constructor({ x, y }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = 50;
+    this.height = 50;
+    this.image = new Image();
+    this.image.src = "./img/mucir.png"
+
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height,
+    );
+  }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
+}
+class Tish {
+  constructor({ x, y }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = 50;
+    this.height = 50;
+    this.image = new Image();
+    this.image.src = "./img/ish.png"
+
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height,
     );
   }
   update() {
@@ -43,6 +116,8 @@ class WaterGun {
     };
     this.width = 50;
     this.height = 50;
+    this.image = new Image();
+    this.image.src = "./img/watergun.png"
 
     this.velocity = {
       x: 0,
@@ -50,13 +125,12 @@ class WaterGun {
     };
   }
   draw() {
-    ctx.fillStyle = "green";
-    ctx.fillRect(
+    ctx.drawImage(
+      this.image,
       this.position.x,
       this.position.y,
       this.width,
       this.height,
-      this.color
     );
   }
   update() {
@@ -73,10 +147,10 @@ class WaterGun {
 
 const inventory = new Inventory();
 const items = [
-  new Items({ x: 50, y: 10 }),
-  new Items({ x: 350, y: 10 }),
-  new Items({ x: 700, y: 10 }),
-  new Items({ x: 200, y: 10 }),
+  new Filter({ x: 50, y: 10 }),
+  new WaterGun({ x: 350, y: 10 }),
+  new Branch({ x: 700, y: 10 }),
+  new Tish({ x: 200, y: 10 }),
 ];
 
 function animate() {
